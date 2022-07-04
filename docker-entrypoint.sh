e7fae9dd730c67787b8997a01eca05f31cbb6dab
smit95tpatel <smit.patel@razorpay.com>
2022-07-05 02:18:25 +0530

    feat: added environment at runtime

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-# set -euo pipefail
 
-# # Capture all environment variables starting with REACT_APP_ and make JSON string from them
-# ENV_JSON="$(jq --compact-output --null-input 'env | with_entries(select(.key | startswith("REACT_APP_")))')"
-
-# # Escape sed replacement's special characters: \, &, /.
-# # No need to escape newlines, because --compact-output already removed them.
-# # Inside of JSON strings newlines are already escaped.
-# ENV_JSON_ESCAPED="$(printf "%s" "${ENV_JSON}" | sed -e 's/[\&/]/\\&/g')"
+# Set environment passed to container at runtime to global variable ENV.
+set -euo pipefail
 
 sed -i "s/<noscript id=\"env-insertion-point\"><\/noscript>/<script>var ENV=\"${APP_ENV}\"<\/script>/g" /app/react/build/index.html
 
