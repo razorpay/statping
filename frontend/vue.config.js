7cb705b8ae0237a702c099b608f4f227aa9e3332
smit95tpatel <smit.patel@razorpay.com>
2022-09-19 16:56:52 +0530

    fix: lint changes

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,32 +1,29 @@
 module.exports = {
-    // baseUrl: '/',
-    devServer: {
-        proxy: 'https://statping.concierge.stage.razorpay.in',
+  baseUrl: "/",
+  assetsDir: "assets",
+  filenameHashing: false,
+  productionTip: process.env.NODE_ENV !== "production",
+  devtools: process.env.NODE_ENV !== "production",
+  performance: process.env.NODE_ENV !== "production",
+  devServer: {
+    disableHostCheck: true,
+    proxyTable: {
+      "/api": {
+        logLevel: "debug",
+        target: "http://0.0.0.0:8585",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+      },
+      "/oauth": {
+        logLevel: "debug",
+        target: "http://0.0.0.0:8585",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/oauth": "",
+        },
+      },
     },
-    // assetsDir: 'assets',
-    // filenameHashing: false,
-    // productionTip: process.env.NODE_ENV !== 'production',
-    // devtools: process.env.NODE_ENV !== 'production',
-    // performance: process.env.NODE_ENV !== 'production',
-    // devServer: {
-    //   disableHostCheck: true,
-    //   proxyTable: {
-    //     '/api': {
-    //       logLevel: 'debug',
-    //       target: 'http://0.0.0.0:8585',
-    //       changeOrigin: true,
-    //       pathRewrite: {
-    //         '^/api': ''
-    //       }
-    //     },
-    //     '/oauth': {
-    //       logLevel: 'debug',
-    //       target: 'http://0.0.0.0:8585',
-    //       changeOrigin: true,
-    //       pathRewrite: {
-    //         '^/oauth': ''
-    //       }
-    //     }
-    //   }
-    // }
+  },
 };
